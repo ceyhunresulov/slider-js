a8f5c55e0eb19abbf7ed7ba9d4a1007718cea427
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-01-11 18:31:00 +0400

    bu layihede slider və qar yağışını birlikdə yazdım. Html/Css və Js istifadə etdim

---
+++ app.js
@@ -0,0 +1,46 @@
+const images = document.getElementById("images");
+const leftBtn = document.getElementById("left-btn");
+const rightBtn = document.getElementById("right-btn");
+
+document.addEventListener("DOMContentLoaded", () => {
+  createSnowIconInContainer();
+});
+
+leftBtn.addEventListener("click", rightScroll);
+
+rightBtn.addEventListener("click", leftScroll);
+
+function createSnowIconInContainer() {
+  const container = document.getElementById("container");
+  const far = document.createElement("i");
+  far.classList.add("far");
+  far.classList.add("fa-snowflake");
+  far.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+  far.style.top = `${-Math.floor(Math.random() * 100)}px`;
+  far.style.fontSize = `${Math.floor(Math.random() * 7)}px`;
+  container.appendChild(far);
+  setTimeout(() => {
+    far.remove();
+  }, 7000);
+}
+setInterval(createSnowIconInContainer, 1);
+
+function rightScroll() {
+  if (images.offsetLeft < 0) {
+    images.style.left = `${images.offsetLeft + window.innerWidth}px`;
+  } else {
+    leftBtn.style.cursor = "not-allowed";
+  }
+
+  rightBtn.style.cursor = "pointer";
+}
+
+function leftScroll() {
+  if (window.innerWidth * (images.children.length - 1) > -images.offsetLeft) {
+    images.style.left = `${images.offsetLeft - window.innerWidth}px`;
+  } else {
+    rightBtn.style.cursor = "not-allowed";
+  }
+
+  leftBtn.style.cursor = "pointer";
+}
